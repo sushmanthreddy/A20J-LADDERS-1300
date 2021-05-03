57ac73d8c60485b9ff3fcb15dfe4106f13f56145
SUSHMANTHREDDY <73489688+SUSHMANTHREDDY@users.noreply.github.com>
2021-05-03 11:56:14 +0530

    Update Queue at the School.cpp

--- Queue at the School.cpp
+++ Queue at the School.cpp
@@ -7,13 +7,13 @@ int main()
 	int n,t;
 	cin>>n>>t;
 	string s;
-    cin>>s;
+	getline(cin,s);
 	for(int i=0;i<t;i++){
 		for(int j=0;j<n;j++){
 			if(s[j]== 'B' && s[j+1]== 'G'){
-        s[j]= 'G';
-				s[j+1]='B';
-				j++;
+                s[j]= 'G';
+	        s[j+1]='B';
+	        j++;
 			}
 		}
 	}
