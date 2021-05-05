e09ba65eab30bae4004762629a01895d51264a8c
SUSHMANTHREDDY <sushmanthreddy@gmail.com>
2021-05-05 13:40:58 +0530

    Stones on the Table

---
+++ Stones on the Table.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+    size_t n;
+    cin >> n;
+    string s;
+    cin >> s;
+    s.erase(unique(s.begin(), s.end()), s.end());
+    cout << n - s.length() << endl;
+    return 0;
+}
