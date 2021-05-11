4a5be43f484be5afbd3721dcc9872f9a928c304c
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-05-11 21:58:25 +0530

    bits++ solution

---
+++ bit++.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,x=0;
+    cin>>n;
+    string s;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        if(s[1] == '+'){
+        ++x;
+        }
+        else{
+            --x;
+        }  
+    }
+    cout<<x<<endl;
+return 0;
+}
