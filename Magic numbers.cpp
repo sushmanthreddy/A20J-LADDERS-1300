00f9d8d04ea4c074e08b37b022265368cbd7f4d6
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-05-15 23:50:27 +0530

    magic numbers

--- Magic numbers.cpp
+++ Magic numbers.cpp
@@ -4,32 +4,37 @@ using namespace std;
 
 int main(){
     string s;
-    cin>>s;
-    bool flag=true;
-    
-    for(int i=0;i=s.length();){
-        if(s[i]=='1' && s[i+1]=='4'  && s[i+2]=='4')
+        bool flag=true;
+
+        cin>>s;
+        for(int i=0;i<s.length();)
+        {
+            if(s[i]=='1' && s[i+1]=='4'  && s[i+2]=='4')
             {
-                i+=3;
+                i+=3;//cout<<"144"<<endl;
             }
-        else if(s[i]=='1' && s[i+1]=='4')
+
+            else if(s[i]=='1' && s[i+1]=='4')
             {
                 i+=2;//cout<<"14"<<endl;
             }
-        else if(s[i]=='1')
+
+            else if(s[i]=='1')
             {
                 i++;//cout<<"14"<<endl;
             }
-        else{
-             flag=false;
-             break;
+
+            else
+            {
+                flag=false;
+                break;
+            }
         }
-    }
-    if (flag=false){
-        cout<<"NO"<<endl;
-    }
-    else{
-        cout<<"yes"<<endl;
-    }
+
+        if(flag)
+            cout<<"YES";
+        else
+            cout<<"NO";
+
     return 0;
 }
