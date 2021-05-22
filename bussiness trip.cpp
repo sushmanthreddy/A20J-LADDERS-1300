4c53a4dec5c2fc9f81748921bd468af308aa49d5
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-05-22 13:00:38 +0530

    bussiness trip

---
+++ bussiness trip.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   int k, a[12], total(0);
+    cin >> k;
+    for (int i = 0; i < 12; ++i)
+    {
+        cin >> a[i];
+        total += a[i];
+    }
+
+    if (total < k)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        sort(a, a + sizeof(a) / sizeof(a[0]), greater<int>());
+        int sum = 0;
+        int month = 0;
+        while (sum < k)
+        {
+            sum += a[month];
+            month += 1;
+        }
+        cout << month << endl;
+    }
+ return 0;
+}
