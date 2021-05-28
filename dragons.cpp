8df4f66f25b2b206858d570445bbe8ab7801540c
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-05-29 00:59:51 +0530

    dragons solution

---
+++ dragons.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+ 
+using namespace  std;
+ 
+int main(){
+    int n,s,a,b;
+    vector <pair <int, int >> v;
+   cin >> s >> n;  
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        v.push_back(make_pair(a, b));
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; i++) {
+        if(s <= v[i].first){
+            cout << "NO";
+            return 0;
+        } else {
+            s += v[i].second;
+        }
+    }
+    cout << "YES"<<endl;
+    return 0;
+}
