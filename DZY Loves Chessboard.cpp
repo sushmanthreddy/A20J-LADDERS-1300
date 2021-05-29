2cf0c4d65823bb29e6d9e2b4a136099d6e93c02a
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-05-29 15:10:29 +0530

    DZY Loves Chessboard

---
+++ DZY Loves Chessboard.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+        int i,j,k;
+        int n,m;
+
+        cin>>n>>m;
+        char a[n][m];
+
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(a[i][j]=='.')
+                {
+                    if((i+j)%2==0)
+                        a[i][j]='B';
+                    else
+                        a[i][j]='W';
+                }
+                else
+                    continue;
+            }
+        }
+
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                cout<<a[i][j];
+            }
+
+            cout<<endl;
+        }
+
+        return 0;
+}
