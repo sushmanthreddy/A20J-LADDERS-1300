06387bebc713fd2f28b470ba141ea9435f4952a8
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-06-09 10:23:33 +0530

    codeforces solution

---
+++ elephant.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=0,count=0;
+    while(i<n){
+        i+=5;
+        count++;
+    }
+    cout<<count<<endl;
+
+}
---
+++ tram.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+     int n;
+	 cin>>n;
+	 int capacity=0;
+	 int stillnow=0;
+	 for(int i=0;i<n;i++){
+		 int a,b;
+		 cin>>a>>b;
+		 stillnow-=a;
+		 stillnow+=b;
+		 capacity=max(capacity,stillnow);
+
+	 }
+	 cout<<capacity;
+
+	return 0;
+}
